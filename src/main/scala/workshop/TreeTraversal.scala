6ac2c139a175554b9c9570911c2830edc3ce89e1
Miroslav Slivka <miroslav.slivka@wandera.com>
2018-11-05 16:27:26 +0100

    first class - tailrec

--- src/main/scala/workshop/TreeTraversal.scala
+++ src/main/scala/workshop/TreeTraversal.scala
@@ -1,17 +1,24 @@
 package workshop
 
+import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 
 object TreeTraversal {
 
   def inorder[A](tree: Tree[A]): List[A] = tree match {
-    case NilNode       => Nil
+    case NilNode => Nil
     case Node(v, l, r) => inorder(l) ::: v :: inorder(r)
   }
 
-  def preorder[A](tree: Tree[A]): List[A] = tree match {
-    case NilNode       => Nil
-    case Node(v, l, r) => v :: preorder(l) ::: preorder(r)
+  def preorder[A](tree: Tree[A]): List[A] = {
+    @tailrec
+    def go(tl: List[Tree[A]], acc: List[A]): List[A] = tl match {
+      case Nil => acc.reverse
+      case NilNode :: tail => go(tail, acc)
+      case Node(v, l, r) :: tail => go(l :: r :: tail, v :: acc)
+    }
+
+    go(List(tree), Nil)
   }
 
   def postorder[A](tree: Tree[A]): List[A] = tree match {
@@ -20,13 +27,14 @@ object TreeTraversal {
   }
 
   def bfsorder[A](tree: Tree[A]): List[A] = {
-    def go(q: Queue[Tree[A]]): List[A] = q.dequeueOption match {
-      case None                        => Nil
-      case Some((NilNode, Queue()))    => Nil
-      case Some((NilNode, rest))       => go(rest)
-      case Some((Node(v, l, r), rest)) => v :: go(rest.enqueue(l).enqueue(r))
+    @tailrec
+    def go(q: Queue[Tree[A]], acc: List[A]): List[A] = q.dequeueOption match {
+      case None => acc.reverse
+      case Some((NilNode, rest)) => go(rest, acc)
+      case Some((Node(v, l, r), rest)) => go(rest.enqueue(l).enqueue(r), v :: acc)
     }
-    go(Queue(tree))
+
+    go(Queue(tree), Nil)
   }
 
   def dfsorder[A](tree: Tree[A]): List[A] = preorder(tree)
