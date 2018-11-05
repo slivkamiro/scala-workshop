e4d0eb53d290185d26927569bc89df4dda68db0b
Miroslav Slivka <mi.slivka@gmail.com>
2018-11-05 19:35:35 +0100

    first class - solutions

--- src/main/scala/workshop/Sorting.scala
+++ src/main/scala/workshop/Sorting.scala
@@ -2,6 +2,9 @@ package workshop
 
 object Sorting {
 
-  def sort(l: List[Int]): List[Int] = ???
+  def sort(l: List[Int]): List[Int] = l match {
+    case Nil            => Nil
+    case head :: tail   => sort(tail.filter(_ <= head)) ::: head :: sort(tail.filter(_ > head))
+  }
 
 }
--- src/main/scala/workshop/TreeTraversal.scala
+++ src/main/scala/workshop/TreeTraversal.scala
@@ -1,15 +1,34 @@
 package workshop
 
+import scala.collection.immutable.Queue
+
 object TreeTraversal {
 
-  def inorder[A](tree: Tree[A]): List[A] = ???
+  def inorder[A](tree: Tree[A]): List[A] = tree match {
+    case NilNode       => Nil
+    case Node(v, l, r) => inorder(l) ::: v :: inorder(r)
+  }
 
-  def preorder[A](tree: Tree[A]): List[A] = ???
+  def preorder[A](tree: Tree[A]): List[A] = tree match {
+    case NilNode       => Nil
+    case Node(v, l, r) => v :: preorder(l) ::: preorder(r)
+  }
 
-  def postorder[A](tree: Tree[A]): List[A] = ???
+  def postorder[A](tree: Tree[A]): List[A] = tree match {
+    case NilNode       => Nil
+    case Node(v, l, r) => (postorder(l) ::: postorder(r)) :+ v
+  }
 
-  def bfsorder[A](tree: Tree[A]): List[A] = ???
+  def bfsorder[A](tree: Tree[A]): List[A] = {
+    def go(q: Queue[Tree[A]]): List[A] = q.dequeueOption match {
+      case None                        => Nil
+      case Some((NilNode, Queue()))    => Nil
+      case Some((NilNode, rest))       => go(rest)
+      case Some((Node(v, l, r), rest)) => v :: go(rest.enqueue(l).enqueue(r))
+    }
+    go(Queue(tree))
+  }
 
-  def dfsorder[A](tree: Tree[A]): List[A] = ???
+  def dfsorder[A](tree: Tree[A]): List[A] = preorder(tree)
 
 }
